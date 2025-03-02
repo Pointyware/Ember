ea829150aed70aea5325005ddbcd02180af4addc
Taush Sampley <taushsampley@gmail.com>
2025-03-02 06:52:40 -0600

    Create root project settings

---
+++ settings.build.kts
@@ -0,0 +1,36 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Artes"
+include(
+    ":app-android",
+    ":app-desktop",
+//    ":app-ios",
+    ":app-shared"
+)
+
+include(
+    ":core-data",
+    ":core-ui",
+)
+
+include(
+    ":feature-text-chat",
+)
+
+include(
+    ":services:open-ai"
+)
