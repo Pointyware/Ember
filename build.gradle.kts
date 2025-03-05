3cfc93c343fd70290ad8efab6a44d5106c153f4d
Taush Sampley <taushsampley@gmail.com>
2025-03-05 06:56:23 -0600

    Add root project build script

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+
+    alias(libs.plugins.kotlinxSerialization) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinCocoapods) apply false
+    alias(libs.plugins.composePlugin) apply false
+}
