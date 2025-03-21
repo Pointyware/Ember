7be22f9232f42e7505e086cba835efb09518c4ca
Taush Sampley <taushsampley@gmail.com>
2025-03-21 06:12:28 -0500

    Generate android main activity

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -1,13 +1,21 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
 
     jvmToolchain(17)
     androidTarget() {
+    }
 
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+                implementation(projects.appShared)
+            }
+        }
     }
 }
 
@@ -18,4 +26,24 @@ android {
     defaultConfig {
         minSdk = 24
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+dependencies {
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activityCompose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.compose.uiToolingPreview)
+    implementation(libs.compose.material3)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.compose.uiTooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ app-android/src/androidMain/kotlin/org/pointyware/artes/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.pointyware.artes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import org.pointyware.artes.ui.theme.ArtesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArtesTheme {
+
+            }
+        }
+    }
+}
