abca9fdbc41e1b94599bb5012902f9619ac95565
Taush Sampley <taushsampley@gmail.com>
2025-03-21 06:14:12 -0500

    Fix coroutine context provide as dependency

--- core-data/src/commonMain/kotlin/org/pointyware/artes/data/di/DataModule.kt
+++ core-data/src/commonMain/kotlin/org/pointyware/artes/data/di/DataModule.kt
@@ -1,21 +1,21 @@
 package org.pointyware.artes.data.di
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
-import kotlin.coroutines.CoroutineContext
 
 val dataQualifier = named("data")
 
 /**
  */
 fun dataModule() = module {
-    single<CoroutineContext>(qualifier = dataQualifier) {
+    single<CoroutineDispatcher>(qualifier = dataQualifier) {
         Dispatchers.IO
     }
     single<CoroutineScope>(qualifier = dataQualifier) {
-        CoroutineScope(get<CoroutineContext>(qualifier = dataQualifier) + SupervisorJob())
+        CoroutineScope(get<CoroutineDispatcher>(qualifier = dataQualifier) + SupervisorJob())
     }
 }
