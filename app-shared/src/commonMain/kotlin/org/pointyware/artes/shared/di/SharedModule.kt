36d0670717fdca3645a5d9a5b7b4290ecf2b7ba6
Taush Sampley <taushsampley@gmail.com>
2025-03-21 06:13:52 -0500

    Provide missing dependencies

--- app-shared/src/commonMain/kotlin/org/pointyware/artes/shared/di/SharedModule.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/artes/shared/di/SharedModule.kt
@@ -1,10 +1,12 @@
 package org.pointyware.artes.shared.di
 
+import io.ktor.client.HttpClient
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.pointyware.artes.data.di.dataModule
 import org.pointyware.artes.data.di.dataQualifier
+import org.pointyware.artes.services.openai.network.openAiHttpClient
 import org.pointyware.artes.text.completion.CompletionViewModel
 
 /**
@@ -22,6 +24,8 @@ fun sharedModule() = module {
 
         sharedViewModelModule()
     )
+
+    single<HttpClient> { openAiHttpClient() }
 }
 
 fun sharedViewModelModule() = module {
--- app-shared/src/desktopMain/kotlin/org/pointyware/artes/shared/di/SharedModule.desktop.kt
+++ app-shared/src/desktopMain/kotlin/org/pointyware/artes/shared/di/SharedModule.desktop.kt
@@ -5,11 +5,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.pointyware.artes.data.TextCompletionService
+import org.pointyware.artes.services.openai.network.OpenAiTextCompletionService
 
 actual fun platformSharedModule(): Module {
     return module {
         single<CoroutineScope>(qualifier = appQualifier) {
             CoroutineScope(SupervisorJob() + Dispatchers.Main)
+        }
+
+        single<TextCompletionService> {
+            OpenAiTextCompletionService(get(), get())
         }
     }
 }
