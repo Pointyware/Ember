30a2b6b37001f27dbee34e293fdd5b414030bc04
Taush Sampley <taushsampley@gmail.com>
2025-04-09 20:02:19 -0500

    Add google ai config

--- services/open-ai/src/commonMain/kotlin/org/pointyware/artes/services/openai/OpenAiConfig.kt
+++ services/open-ai/src/commonMain/kotlin/org/pointyware/artes/services/openai/OpenAiConfig.kt
@@ -1,11 +1,12 @@
 package org.pointyware.artes.services.openai
 
+import org.pointyware.artes.entities.Google
 import org.pointyware.artes.entities.Host
 import org.pointyware.artes.entities.HostConfig
 import org.pointyware.artes.entities.OpenAi
 
 /**
- *
+ * A user's configuration for OpenAI.
  */
 class OpenAiConfig(
     override val id: Long,
@@ -16,4 +17,19 @@ class OpenAiConfig(
 
     override val host: Host
         get() = OpenAi
+}
+
+/**
+ * A user's account configuration for Gemini.
+ *
+ * TODO: move to Google module
+ */
+class GeminiConfig(
+    override val id: Long,
+    override val title: String,
+    val apiKey: String,
+): HostConfig {
+
+    override val host: Host
+        get() = Google
 }
