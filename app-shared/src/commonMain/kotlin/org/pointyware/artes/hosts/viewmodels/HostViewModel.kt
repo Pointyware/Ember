0bb3c7eda7bdaacebf0ba2a78f00c7ab99833bc8
Taush Sampley <taushsampley@gmail.com>
2025-04-16 20:01:46 -0500

    pass configuration extras as single parameter

--- app-shared/src/commonMain/kotlin/org/pointyware/artes/hosts/viewmodels/HostViewModel.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/artes/hosts/viewmodels/HostViewModel.kt
@@ -2,8 +2,13 @@ package org.pointyware.artes.hosts.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.pointyware.artes.hosts.interactors.CreateHostUseCase
+import org.pointyware.artes.viewmodels.LoadingUiState
 
 /**
  *
@@ -12,17 +17,33 @@ class HostViewModel(
     private val createHostUseCase: CreateHostUseCase,
 ): ViewModel() {
 
-    // TODO: add state for alert/dialog/toast/snackbar to notify of success
+    private val _state = MutableStateFlow(HostConfigUiState(loading = LoadingUiState.Idle))
+    val state: StateFlow<HostConfigUiState> get() = _state.asStateFlow()
 
-    fun createHost(title: String, orgId: String, key: String) {
+    fun createHost(title: String, extraOptions: ExtraOptionsUiState) {
         viewModelScope.launch {
+            val orgId = when (extraOptions) {
+                is ExtraOptionsUiState.OpenAi -> extraOptions.orgId
+                is ExtraOptionsUiState.Gemini -> ""
+            }
+            val key = when (extraOptions) {
+                is ExtraOptionsUiState.OpenAi -> extraOptions.apiKey
+                is ExtraOptionsUiState.Gemini -> extraOptions.apiKey
+            }
             println("Creating host with title: $title, orgId: $orgId, key: ${"*".repeat(key.length)}")
+            _state.update {
+                it.copy(loading = LoadingUiState.Loading)
+            }
             createHostUseCase(title, orgId, key)
                 .onSuccess {
-                    println("Host created successfully")
+                    _state.update {
+                        it.copy(loading = LoadingUiState.Idle)
+                    }
                 }
-                .onFailure {
-                    println("Failed to create host: ${it.message}")
+                .onFailure { error ->
+                    _state.update {
+                        it.copy(loading = LoadingUiState.Error(error.message ?: "Unknown error"))
+                    }
                 }
         }
     }
