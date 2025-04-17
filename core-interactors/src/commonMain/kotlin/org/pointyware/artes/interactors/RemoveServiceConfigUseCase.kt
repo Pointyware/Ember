20496ae409354e0ca916254e6c653265f7d29fcb
Taush Sampley <taushsampley@gmail.com>
2025-04-17 00:07:03 -0500

    Implement use case

--- core-interactors/src/commonMain/kotlin/org/pointyware/artes/interactors/RemoveServiceConfigUseCase.kt
+++ core-interactors/src/commonMain/kotlin/org/pointyware/artes/interactors/RemoveServiceConfigUseCase.kt
@@ -1,5 +1,12 @@
 package org.pointyware.artes.interactors
 
-class RemoveServiceConfigUseCase {
+import org.pointyware.artes.data.hosts.ServiceRepository
 
+class RemoveServiceConfigUseCase(
+    private val serviceConfigRepository: ServiceRepository,
+) {
+
+    suspend fun invoke(id: Long): Result<Unit> = runCatching {
+        serviceConfigRepository.removeService(id)
+    }
 }
