82f3fee5d6478cf6dec15a4c09eb96b0606af29c
Taush Sampley <taushsampley@gmail.com>
2025-04-17 17:50:59 -0500

    Add navigation tabs

--- app-shared/src/commonMain/kotlin/org/pointyware/artes/shared/ui/ArtesApp.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/artes/shared/ui/ArtesApp.kt
@@ -1,8 +1,14 @@
 package org.pointyware.artes.shared.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import org.pointyware.artes.navigation.Destination
 
 /**
  * The root for Compose UI.
@@ -10,7 +16,27 @@ import androidx.compose.ui.Modifier
 @Composable
 fun ArtesApp() {
 
-    AgentServiceNavigation(
-        modifier = Modifier.fillMaxSize()
-    )
+    Column {
+        val navController = rememberNavController()
+        Row {
+            Button(
+                onClick = {
+                    navController.navigate(Destination.AgentList)
+                }
+            ) {
+                Text("Agents")
+            }
+            Button(
+                onClick = {
+                    navController.navigate(Destination.ServiceList)
+                }
+            ) {
+                Text("Services")
+            }
+        }
+        AgentServiceNavigation(
+            modifier = Modifier.weight(1f),
+            navController = navController
+        )
+    }
 }
