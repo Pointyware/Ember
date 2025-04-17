e96356e008685adfdc1db4beb83f874028279758
Taush Sampley <taushsampley@gmail.com>
2025-04-17 17:04:43 -0500

    Replace AgentInfoViewState.service with HostUiState type

--- core-ui/src/commonMain/kotlin/org/pointyware/artes/ui/AgentInfoView.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/ui/AgentInfoView.kt
@@ -10,11 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import org.pointyware.artes.viewmodels.HostUiState
 
 data class AgentInfoViewState(
     val name: String,
     val description: String,
-    val service: ServiceListItemState
+    val service: HostUiState
 )
 
 /**
