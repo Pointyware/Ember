3c57e965b6cfd391e4675fa00840dfb3bc0f79c3
Taush Sampley <taushsampley@gmail.com>
2025-04-17 00:10:18 -0500

    Provide default arguments

--- core-viewmodels/src/commonMain/kotlin/org/pointyware/artes/viewmodels/HostConfigListUiState.kt
+++ core-viewmodels/src/commonMain/kotlin/org/pointyware/artes/viewmodels/HostConfigListUiState.kt
@@ -3,6 +3,6 @@ package org.pointyware.artes.viewmodels
 /**
  */
 data class HostConfigListUiState(
-    val hostConfigs: List<HostUiState>,
-    val loadingUiState: LoadingUiState
+    val hostConfigs: List<HostUiState> = emptyList(),
+    val loadingUiState: LoadingUiState = LoadingUiState.Idle
 )
