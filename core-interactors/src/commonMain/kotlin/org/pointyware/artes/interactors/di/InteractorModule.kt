67f9a8bfb4e2a6178f8323515bd8cd9580d6966c
Taush Sampley <taushsampley@gmail.com>
2025-04-16 23:17:59 -0500

    Provide missing dependencies

--- core-interactors/src/commonMain/kotlin/org/pointyware/artes/interactors/di/InteractorModule.kt
+++ core-interactors/src/commonMain/kotlin/org/pointyware/artes/interactors/di/InteractorModule.kt
@@ -2,6 +2,8 @@ package org.pointyware.artes.interactors.di
 
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
+import org.pointyware.artes.interactors.BeginAgentCreationJourney
+import org.pointyware.artes.interactors.GetAgentListUseCase
 import org.pointyware.artes.interactors.agents.GetAgentUseCase
 import org.pointyware.artes.interactors.hosts.GetHostServicesUseCase
 
@@ -11,4 +13,6 @@ import org.pointyware.artes.interactors.hosts.GetHostServicesUseCase
 fun interactorsModule() = module {
     factoryOf(::GetHostServicesUseCase)
     factoryOf(::GetAgentUseCase)
+    factoryOf(::GetAgentListUseCase)
+    factoryOf(::BeginAgentCreationJourney)
 }
