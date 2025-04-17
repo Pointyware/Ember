8d79df05948c6747d8dcf0b74894cee8ed1469ef
Taush Sampley <taushsampley@gmail.com>
2025-04-17 17:07:23 -0500

    Add destinations from desktop reference app

--- core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
@@ -2,10 +2,17 @@ package org.pointyware.artes.navigation
 
 /**
  * Core navigation destinations.
+ *
+ * The last four described navigation of the desktop reference application. These will be merged.
  */
 enum class Destination {
     AgentList,
     AgentInfo,
     AgentEditor,
-    ServiceList
+    ServiceList,
+
+    Workbench,
+    Lab,
+    Dashboard,
+    Chat
 }
