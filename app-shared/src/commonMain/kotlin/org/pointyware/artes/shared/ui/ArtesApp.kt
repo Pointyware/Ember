c04d8e51ec98c84cf66dc7910f42575dc0fb7da2
Taush Sampley <taushsampley@gmail.com>
2025-04-17 23:07:54 -0500

    Use scaffold to host navigation buttons

--- app-shared/src/commonMain/kotlin/org/pointyware/artes/shared/ui/ArtesApp.kt
+++ app-shared/src/commonMain/kotlin/org/pointyware/artes/shared/ui/ArtesApp.kt
@@ -1,41 +1,70 @@
 package org.pointyware.artes.shared.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.pointyware.artes.navigation.Destination
+import org.pointyware.artes.ui.Res
+import org.pointyware.artes.ui.acc_desc_agents
+import org.pointyware.artes.ui.acc_desc_services
+import org.pointyware.artes.ui.agent_24
+import org.pointyware.artes.ui.services_24
+import org.pointyware.artes.ui.title_app
 
 /**
  * The root for Compose UI.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArtesApp() {
-
-    Column {
-        val navController = rememberNavController()
-        Row {
-            Button(
-                onClick = {
-                    navController.navigate(Destination.AgentList.name)
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text =
+                        stringResource(Res.string.title_app)
+                    )
+                },
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                IconButton(
+                    onClick = {
+                        navController.navigate(Destination.AgentList.name)
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.agent_24),
+                        contentDescription = stringResource(Res.string.acc_desc_agents)
+                    )
                 }
-            ) {
-                Text("Agents")
-            }
-            Button(
-                onClick = {
-                    navController.navigate(Destination.ServiceList.name)
+                IconButton(
+                    onClick = {
+                        navController.navigate(Destination.ServiceList.name)
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(Res.drawable.services_24),
+                        contentDescription = stringResource(Res.string.acc_desc_services)
+                    )
                 }
-            ) {
-                Text("Services")
             }
         }
+    ) { scaffoldPadding ->
         AgentServiceNavigation(
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.padding(scaffoldPadding),
             navController = navController
         )
     }
