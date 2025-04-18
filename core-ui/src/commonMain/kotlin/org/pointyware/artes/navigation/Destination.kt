e4ded1d376fa2e175565fc046768152094926718
Taush Sampley <taushsampley@gmail.com>
2025-04-18 05:10:28 -0500

    Add serialization data to destinations

--- core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
@@ -1,19 +1,37 @@
 package org.pointyware.artes.navigation
 
+import kotlinx.serialization.Serializable
+
 /**
  * Core navigation destinations.
  *
  * The last four described navigation of the desktop reference application. These will be merged.
  */
-enum class Destination {
-    AgentList,
-    AgentInfo,
-    AgentEditor,
-    ServiceList,
-    ServiceEditor,
+@Serializable
+sealed interface Destination {
+    @Serializable
+    data object AgentList: Destination
+    @Serializable
+    data class AgentInfo(
+        val id: Long
+    ): Destination
+    @Serializable
+    data class AgentEditor(
+        val id: Long? = null
+    ): Destination
+    @Serializable
+    data object ServiceList: Destination
+    @Serializable
+    data class ServiceEditor(
+        val id: Long? = null
+    ): Destination
 
-    Workbench,
-    Lab,
-    Dashboard,
-    Chat
+    @Serializable
+    data object Workbench: Destination
+    @Serializable
+    data object Lab: Destination
+    @Serializable
+    data object Dashboard: Destination
+    @Serializable
+    data object Chat: Destination
 }
