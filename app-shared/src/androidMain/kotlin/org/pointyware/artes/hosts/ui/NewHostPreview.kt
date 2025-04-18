8955432d69bcd85d6482a1d52f14e9b9f96b3b2c
Taush Sampley <taushsampley@gmail.com>
2025-04-18 03:52:11 -0500

    Add theme and surface to preview

--- app-shared/src/androidMain/kotlin/org/pointyware/artes/hosts/ui/NewHostPreview.kt
+++ app-shared/src/androidMain/kotlin/org/pointyware/artes/hosts/ui/NewHostPreview.kt
@@ -2,27 +2,33 @@ package org.pointyware.artes.hosts.ui
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.pointyware.artes.hosts.viewmodels.ExtraOptionsUiState
 import org.pointyware.artes.hosts.viewmodels.HostConfigEditorUiState
+import org.pointyware.artes.ui.theme.ArtesTheme
 import org.pointyware.artes.viewmodels.LoadingUiState
 
-@Preview(name = "", showBackground = true)
-@Preview(name = " (Night)", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(name = "New Host", showBackground = true)
+@Preview(name = "New Host (Night)", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun NewHostPreview() {
-    HostEditorView(
-        state = HostConfigEditorUiState(
-            title = "Some Host",
-            extraOptions = ExtraOptionsUiState.OpenAi(
-                orgId = "0987654321",
-                apiKey = "1234567890",
-            ),
-            loading = LoadingUiState.Idle,
-        ),
-        modifier = Modifier.fillMaxSize(),
-        onCreateHost = { _, _ -> }
-    )
+    ArtesTheme {
+        Surface {
+            HostEditorView(
+                state = HostConfigEditorUiState(
+                    title = "Some Host",
+                    extraOptions = ExtraOptionsUiState.OpenAi(
+                        orgId = "0987654321",
+                        apiKey = "1234567890",
+                    ),
+                    loading = LoadingUiState.Idle,
+                ),
+                modifier = Modifier.fillMaxSize(),
+                onCreateHost = { _, _ -> }
+            )
+        }
+    }
 }
