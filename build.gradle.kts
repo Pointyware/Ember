99ca91a238610acd296a53a847c8327d501b8faf
Taush Sampley <taushsampley@gmail.com>
2025-04-19 03:12:02 -0500

    configure kover with example ruleset

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinAndroid) apply false
 
-    alias(libs.plugins.kotlinxKover) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
@@ -13,4 +12,38 @@ plugins {
 
     alias(libs.plugins.sqlDelight) apply false
     alias(libs.plugins.ksp) apply false
+
+    alias(libs.plugins.kotlinxKover)
+}
+
+dependencies {
+    kover(projects.coreEntities)
+    kover(projects.coreData)
+    kover(projects.coreViewmodels)
+    kover(projects.coreUi)
+    kover(projects.coreData)
+
+    kover(projects.featureTextChat)
+    kover(projects.services.openAi)
+
+    kover(projects.appShared)
+
+    kover(projects.appAndroid)
+    kover(projects.appDesktop)
+}
+
+kover.reports {
+    filters {
+        excludes.classes("kotlinx.kover.examples.merged.utils.*", "kotlinx.kover.examples.merged.subproject.utils.*")
+        includes.classes("kotlinx.kover.examples.merged.*")
+    }
+
+    verify {
+        rule {
+            bound {
+                minValue.set(50)
+                maxValue.set(75)
+            }
+        }
+    }
 }
