1cbaebf85438f24a4dc20b9c7c3d049889485cde
Taush Sampley <taushsampley@gmail.com>
2025-06-08 23:31:13 -0500

    Enable core-common module

--- core-common/build.gradle.kts
+++ core-common/build.gradle.kts
@@ -1,9 +1,39 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinxKover)
 }
 
 kotlin {
     jvmToolchain(21)
 
+    jvm("desktop")
+    androidTarget() {
+
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.koin.core)
+                implementation(libs.koin.coroutines)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+                implementation(libs.kotlinx.coroutinesTest)
+                implementation(libs.truth)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "org.pointyware.artes.common"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
 }
