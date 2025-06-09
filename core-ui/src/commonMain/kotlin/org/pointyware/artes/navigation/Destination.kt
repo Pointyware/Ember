c8cfdcfedf9966f5725eadf0daabf37b02d6c5e7
Taush Sampley <taushsampley@gmail.com>
2025-06-09 02:07:06 -0500

    Space entries to ease readability

--- core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
@@ -11,16 +11,20 @@ import kotlinx.serialization.Serializable
 sealed interface Destination {
     @Serializable
     data object AgentList: Destination
+
     @Serializable
     data class AgentInfo(
         val id: Long
     ): Destination
+
     @Serializable
     data class AgentEditor(
         val id: Long? = null
     ): Destination
+
     @Serializable
     data object ServiceList: Destination
+
     @Serializable
     data class ServiceEditor(
         val id: Long? = null
