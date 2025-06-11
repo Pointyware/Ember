f365788948e7a7af7e5951f17a2673e13eccf872
Taush Sampley <taushsampley@gmail.com>
2025-06-10 21:27:24 -0500

    Create loss function interface

---
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/LossFunction.kt
@@ -0,0 +1,10 @@
+package org.pointyware.artes.scratch.loss
+
+import org.pointyware.artes.scratch.Tensor
+
+/**
+ * A loss function computes the error between the predicted output and the target output.
+ */
+interface LossFunction {
+    fun compute(predicted: Tensor, target: Tensor): Tensor
+}
