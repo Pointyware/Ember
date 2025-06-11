3b9503f225afe57b0c6a21f2235cd16ac7080713
Taush Sampley <taushsampley@gmail.com>
2025-06-10 23:38:00 -0500

    Update loss function import

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/LossFunction.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/LossFunction.kt
@@ -1,6 +1,6 @@
 package org.pointyware.artes.scratch.loss
 
-import org.pointyware.artes.scratch.Tensor
+import org.pointyware.artes.scratch.tensors.Tensor
 
 /**
  * A loss function computes the error between the predicted output and the target output.
