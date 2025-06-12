a07a1f41cfe4c9ffd28a13a8b709a496b2407a43
Taush Sampley <taushsampley@gmail.com>
2025-06-12 13:19:56 -0500

    Add sigmoid activation stub

---
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/activations/Sigmoid.kt
@@ -0,0 +1,12 @@
+package org.pointyware.artes.scratch.activations
+
+import org.pointyware.artes.scratch.tensors.Tensor
+
+/**
+ *
+ */
+object Sigmoid: ActivationFunction {
+    override fun calculate(input: Tensor): Tensor {
+        TODO("Not yet implemented")
+    }
+}
