8931a9023060fb3d02b410fdafc34cac71e03a86
Taush Sampley <taushsampley@gmail.com>
2025-06-12 16:30:13 -0500

    Add SGD docs and requirement

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/optimizers/StochasticGradientDescent.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/optimizers/StochasticGradientDescent.kt
@@ -3,9 +3,18 @@ package org.pointyware.artes.scratch.optimizers
 import org.pointyware.artes.scratch.layers.Layer
 import org.pointyware.artes.scratch.tensors.Tensor
 
+/**
+ * Stochastic Gradient Descent optimizer with adjustable learning rate.
+ *
+ * @param learningRate The learning rate for the optimizer.
+ */
 class StochasticGradientDescent(
     val learningRate: Double
 ): Optimizer {
+
+    init {
+        require(learningRate > 0) { "Learning rate must be positive." }
+    }
 
     override fun reset(layer: Layer) {
         TODO("Not yet implemented")
