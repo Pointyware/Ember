c009838b95544738eeeb8343769d884b06174802
Taush Sampley <taushsampley@gmail.com>
2025-06-11 21:45:19 -0500

    Stub common loss functions

---
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/MeanSquaredError.kt
@@ -0,0 +1,9 @@
+package org.pointyware.artes.scratch.loss
+
+import org.pointyware.artes.scratch.tensors.Tensor
+
+object MeanSquaredError : LossFunction {
+    override fun compute(expected: Tensor, actual: Tensor): Double {
+        TODO()
+    }
+}
