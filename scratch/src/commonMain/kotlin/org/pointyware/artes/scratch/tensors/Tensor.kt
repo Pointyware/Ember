14777e2dd3749649628118f2ea449cf1ee37e6eb
Taush Sampley <taushsampley@gmail.com>
2025-06-13 17:33:26 -0500

    Add convenience functions for manipulating tensors

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/tensors/Tensor.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/tensors/Tensor.kt
@@ -1,5 +1,10 @@
 package org.pointyware.artes.scratch.tensors
 
+inline fun absoluteIndex(dimensions: List<Int>, indices: IntArray): Int {
+    require(indices.size == dimensions.size) { "Indices must match the tensor order." }
+    return indices.foldIndexed(0) { index, acc, offset -> acc * dimensions[index] + offset }
+}
+
 /**
  * A tensor is a generalization of vectors and matrices to higher dimensions.
  */
@@ -15,6 +20,13 @@ interface Tensor {
 
     operator fun set(index: Int, value: Double)
     operator fun get(index: Int): Double
+
+    operator fun set(indices: IntArray, value: Double) {
+        set(absoluteIndex(dimensions, indices), value)
+    }
+    operator fun get(indices: IntArray): Double {
+        return get(absoluteIndex(dimensions, indices))
+    }
 
     /**
      * Performs element-wise multiplication or matrix multiplication depending on the context.
@@ -45,4 +57,14 @@ interface Tensor {
     operator fun plus(other: Tensor): Tensor
 
     fun computeLoss(): Double
+
+    /**
+     * Applies a function to each element of the tensor, modifying it in place.
+     */
+    fun mapEach(function: (Double)->Double)
+
+    /**
+     * Applies a function to each element of the tensor, modifying it in place.
+     */
+    fun mapEachIndexed(function: (IntArray, Double)->Double)
 }
