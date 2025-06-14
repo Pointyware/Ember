7e86fba9f3fb7735af918a7c0c288eb4c036d1c5
Taush Sampley <taushsampley@gmail.com>
2025-06-14 02:26:54 -0500

    Implement cross entropy loss and mean squared error cost functions

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/CrossEntropyLoss.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/CrossEntropyLoss.kt
@@ -1,9 +1,19 @@
 package org.pointyware.artes.scratch.loss
 
+import org.pointyware.artes.scratch.tensors.SimpleTensor
 import org.pointyware.artes.scratch.tensors.Tensor
+import kotlin.math.ln
 
 object CrossEntropyLoss : LossFunction {
     override fun compute(expected: Tensor, actual: Tensor): Double {
-        TODO()
+        val logProbs = SimpleTensor.from(*actual.dimensions).apply {
+            mapEachIndexed { indices, _ ->
+                // Adding a small constant to avoid log(0)
+                -ln(actual[indices] + 1e-15) * expected[indices]
+            }
+        }
+
+        val basis = actual.totalSize / actual.basisSize
+        return logProbs.values.asSequence().sum() / basis
     }
 }
--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/MeanSquaredError.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/MeanSquaredError.kt
@@ -1,9 +1,29 @@
 package org.pointyware.artes.scratch.loss
 
+import org.pointyware.artes.scratch.tensors.SimpleTensor
 import org.pointyware.artes.scratch.tensors.Tensor
 
 object MeanSquaredError : LossFunction {
     override fun compute(expected: Tensor, actual: Tensor): Double {
-        TODO()
+        require(actual.dimensions.size == expected.dimensions.size) {
+            "Expected and actual tensors must have the same dimensions. %i != %i".format(
+                actual.dimensions, expected.dimensions
+            )
+        }
+
+        val diffs = SimpleTensor(actual.dimensions).apply {
+            mapEachIndexed { indices, _ ->
+                actual[indices] - expected[indices]
+            }
+        }
+        val squared = SimpleTensor(actual.dimensions).apply {
+            mapEachIndexed { indices, _ ->
+                diffs[indices] * diffs[indices]
+            }
+        }
+
+        val mean = squared.values.asSequence().average()
+
+        return mean
     }
 }
