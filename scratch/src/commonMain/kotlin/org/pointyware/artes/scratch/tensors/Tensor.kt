14962f12e3854f1509880f2a4debc6dbb396e655
Taush Sampley <taushsampley@gmail.com>
2025-06-14 18:35:45 -0500

    Generate equals and hashCode for IntArray properties

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/tensors/Tensor.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/tensors/Tensor.kt
@@ -231,6 +231,24 @@ data class Tensor(
         }
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Tensor
+
+        if (!dimensions.contentEquals(other.dimensions)) return false
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = dimensions.contentHashCode()
+        result = 31 * result + data.contentHashCode()
+        return result
+    }
+
     // endregion
 
     companion object {
