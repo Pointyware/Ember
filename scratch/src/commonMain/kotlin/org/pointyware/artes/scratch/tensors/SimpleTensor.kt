8b640617b800fa8bddb6a8d0b111788740631a14
Taush Sampley <taushsampley@gmail.com>
2025-06-14 01:58:24 -0500

    Remove unused signatures

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/tensors/SimpleTensor.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/tensors/SimpleTensor.kt
@@ -19,48 +19,21 @@ class SimpleTensor(
         return data[index]
     }
 
-//    override val dimensions: List<Int> = run {
-//            val dims = mutableListOf<Int>()
-//            var current: Any? = data
-//            while (current is Array<*>) {
-//                dims.add(current.size)
-//                current = if (current.isNotEmpty()) current[0] else null
-//            }
-//            dims.toList()
-//        }
-//
-
-    override fun plus(other: Tensor): Tensor {
-        TODO("Not yet implemented")
-    }
-
-    override fun times(other: Tensor): Tensor {
-        TODO("Not yet implemented")
-    }
-
-    override fun computeLoss(): Double {
-        TODO("Not yet implemented")
-    }
-
     companion object {
         fun from(data: Double): SimpleTensor {
-            return SimpleTensor(emptyList(), data)
+            return SimpleTensor(intArrayOf(), data)
         }
 
         fun from(vector: DoubleArray): SimpleTensor {
-            return SimpleTensor(listOf(vector.size)).apply {
+            return SimpleTensor(intArrayOf(vector.size)).apply {
                 for (i in vector.indices) {
                     this[i] = vector[i]
                 }
             }
         }
 
-        fun from(width: Int): SimpleTensor {
-            return SimpleTensor(listOf(width))
-        }
-
-        fun from(width: Int, height: Int): SimpleTensor {
-            return SimpleTensor(listOf(width, height))
+        fun from(vararg dimensions: Int): SimpleTensor {
+            return SimpleTensor(dimensions)
         }
     }
 }
