37481683128fd1ec303c35d9c79beafbb5087c0e
Taush Sampley <taushsampley@gmail.com>
2025-06-15 12:53:32 -0500

    simplify CrossEntropyLoss

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/CrossEntropyLoss.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/loss/CrossEntropyLoss.kt
@@ -5,11 +5,8 @@ import kotlin.math.ln
 
 object CrossEntropyLoss : LossFunction {
     override fun compute(expected: Tensor, actual: Tensor): Double {
-        val logProbs = Tensor.shape(*actual.dimensions).apply {
-            mapEachFlatIndexed { index, _ ->
-                // Adding a small constant to avoid log(0)
-                -ln(actual[index] + 1e-15) * expected[index]
-            }
+        val logProbs = Tensor.shape(*actual.dimensions).mapEachFlatIndexed { index, _ ->
+            -ln(actual[index] + 1e-15) * expected[index]
         }
 
         return logProbs.values.asSequence().sum() / actual.area
