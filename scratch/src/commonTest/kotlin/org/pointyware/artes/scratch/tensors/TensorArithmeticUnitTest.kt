3ca4ad7df15f601ceca12897a02722146746dab5
Taush Sampley <taushsampley@gmail.com>
2025-06-16 18:43:56 -0500

    Add tensor arithmetic unit tests

---
+++ scratch/src/commonTest/kotlin/org/pointyware/artes/scratch/tensors/TensorArithmeticUnitTest.kt
@@ -0,0 +1,66 @@
+package org.pointyware.artes.scratch.tensors
+
+import org.junit.Test
+import kotlin.math.exp
+import kotlin.test.assertContentEquals
+
+class TensorArithmeticUnitTest {
+
+    @Test
+    fun scalar_addition_returns_new_tensor() {
+        val tensor1 = Tensor.from(
+            doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+            2, 2
+        )
+        val tensor2 = Tensor.from(
+            doubleArrayOf(1.0, 1.0, 1.0, 1.0),
+            2, 2
+        )
+        val result = tensor1 + tensor2
+
+        val expected = Tensor.from(
+            doubleArrayOf(2.0, 3.0, 4.0, 5.0),
+            2, 2
+        )
+        assertContentEquals(expected.data, result.data)
+        assertContentEquals(
+            doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+            tensor1.data,
+            "Operand 1 data should remain unchanged"
+        )
+        assertContentEquals(
+            doubleArrayOf(1.0, 1.0, 1.0, 1.0),
+            tensor2.data,
+            "Operand 2 data should remain unchanged"
+        )
+    }
+
+    @Test
+    fun scalar_subtraction_returns_new_tensor() {
+        val tensor1 = Tensor.from(
+            doubleArrayOf(5.0, 6.0, 7.0, 8.0),
+            4, 1
+        )
+        val tensor2 = Tensor.from(
+            doubleArrayOf(1.0, 1.0, 1.0, 1.0),
+            4, 1
+        )
+        val result = tensor1 - tensor2
+
+        val expected = Tensor.from(
+            doubleArrayOf(4.0, 5.0, 6.0, 7.0),
+            4, 1
+        )
+        assertContentEquals(expected.data, result.data)
+        assertContentEquals(
+            doubleArrayOf(5.0, 6.0, 7.0, 8.0),
+            tensor1.data,
+            "Operand 1 data should remain unchanged"
+        )
+        assertContentEquals(
+            doubleArrayOf(1.0, 1.0, 1.0, 1.0),
+            tensor2.data,
+            "Operand 2 data should remain unchanged"
+        )
+    }
+}
