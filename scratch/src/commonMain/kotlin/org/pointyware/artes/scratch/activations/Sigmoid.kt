70eb78c23685f8acb69f3806ba419bde29f4de6d
Taush Sampley <taushsampley@gmail.com>
2025-06-18 23:24:00 -0500

    Implement sigmoid scalar activation

--- scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/activations/Sigmoid.kt
+++ scratch/src/commonMain/kotlin/org/pointyware/artes/scratch/activations/Sigmoid.kt
@@ -1,15 +1,20 @@
 package org.pointyware.artes.scratch.activations
 
+import kotlin.math.exp
+
 /**
  * Logistic function, also known as "the" sigmoid function; although,
  * a sigmoid function is any function that has an "S" shaped curve.
  */
 object Sigmoid: ScalarActivationFunction {
     override fun scalarActivation(input: Double): Double {
-        TODO("Not yet implemented")
+        // Sigmoid function: f(x) = 1 / (1 + exp(-x))
+        return 1.0 / (1.0 + exp(-input))
     }
 
     override fun scalarDerivative(input: Double): Double {
-        TODO("Not yet implemented")
+        // Derivative of sigmoid: f'(x) = f(x) * (1 - f(x))
+        val sigmoidValue = scalarActivation(input)
+        return sigmoidValue * (1.0 - sigmoidValue)
     }
 }
