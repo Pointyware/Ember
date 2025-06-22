8fccd093bcb993a117c6626340fec0e6304eb416
Taush Sampley <taushsampley@gmail.com>
2025-06-22 18:53:23 -0500

    Remove services modules

--- services/open-ai/src/androidMain/kotlin/org/pointyware/artes/services/openai/network/OpenAiCredentials.android.kt
+++
@@ -1,8 +0,0 @@
-package org.pointyware.artes.services.openai.network
-
-/**
- * OpenAI Credentials would not be deployed with an app so we won't load from resources or assets.
- * Credentials would be configured by a user in an app and passed to our API.
- *
- *
- */
