4f1a5ea47939917b7e894ae402e5e318156299ef
Taush Sampley <taushsampley@gmail.com>
2025-06-23 17:09:50 -0500

    Remove unused imports

--- core-viewmodels/src/commonMain/kotlin/org/pointyware/artes/viewmodels/di/ViewModelModule.kt
+++ core-viewmodels/src/commonMain/kotlin/org/pointyware/artes/viewmodels/di/ViewModelModule.kt
@@ -1,12 +1,6 @@
 package org.pointyware.artes.viewmodels.di
 
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
-import org.pointyware.artes.viewmodels.AgentListViewModel
-import org.pointyware.artes.viewmodels.DefaultAgentListViewModel
-import org.pointyware.artes.viewmodels.DefaultServiceListViewModel
-import org.pointyware.artes.viewmodels.ServiceListViewModel
 
 /**
  *
