a6229857ce3d0c7d89638eeff2decb19d204a28c
Taush Sampley <taushsampley@gmail.com>
2025-06-22 19:04:30 -0500

    Remove all destinations defined for artes

--- core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
@@ -9,33 +9,4 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 sealed interface Destination {
-    @Serializable
-    data object AgentList: Destination
-
-    @Serializable
-    data class AgentInfo(
-        val id: Long
-    ): Destination
-
-    @Serializable
-    data class AgentEditor(
-        val id: Long? = null
-    ): Destination
-
-    @Serializable
-    data object ServiceList: Destination
-
-    @Serializable
-    data class ServiceEditor(
-        val id: Long? = null
-    ): Destination
-
-    @Serializable
-    data object Workbench: Destination
-    @Serializable
-    data object Lab: Destination
-    @Serializable
-    data object Dashboard: Destination
-    @Serializable
-    data object Chat: Destination
 }
