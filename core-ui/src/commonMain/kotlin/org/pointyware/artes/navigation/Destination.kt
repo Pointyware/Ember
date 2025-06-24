2dc9ae588e09933eb3d63523139fcff0ad40b06f
Taush Sampley <taushsampley@gmail.com>
2025-06-24 16:07:11 -0500

    Differentiate between different types of destinations

--- core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/Destination.kt
@@ -5,7 +5,11 @@ import kotlinx.serialization.Serializable
 /**
  * Core navigation destinations.
  *
- * The last four described navigation of the desktop reference application. These will be merged.
+ * These are preferably exposed in a core-navigation module so that each feature can declare
+ * its own destinations without needing to know about others except dependent features.
+ *
+ * These destinations are application-level logic and are distinct from the [TopLevelDestination]
+ * which is a UI-specific implementation of controlling navigation.
  */
 @Serializable
 sealed interface Destination {
--- core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/TopLevelDestination.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/artes/navigation/TopLevelDestination.kt
@@ -4,12 +4,14 @@ import androidx.navigation.NavController
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.StringResource
 import org.pointyware.ember.ui.Res
-import org.pointyware.ember.ui.acc_desc_back
 import org.pointyware.ember.ui.agent_24
-import org.pointyware.ember.ui.title_app
+import org.pointyware.ember.ui.title_destination_lab
+import org.pointyware.ember.ui.acc_desc_lab
 
 /**
  * Defines the top-level destinations in the app that will be shown in the bottom navigation bar.
+ *
+ * @param destination The application-level destination that this top-level destination represents.
  */
 enum class TopLevelDestination(
     val destination: Destination,
