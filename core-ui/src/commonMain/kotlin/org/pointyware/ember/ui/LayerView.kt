31c4d20655f3dfd3b0d3141804c811c1cda92edd
Taush Sampley <taushsampley@gmail.com>
2025-07-07 13:54:57 -0500

    Add interpolated color map

--- core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/LayerView.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/LayerView.kt
@@ -47,17 +47,14 @@ fun NeuronView(
     weights: List<Float>,
     bias: Float,
     modifier: Modifier = Modifier,
-    squareSize: Float = 20f
+    squareSize: Float = 20f,
+    colorMap: ColorMap = DefaultColorMap
 ) {
     Canvas(
         modifier = modifier.width(((weights.size + 1) * 20).dp) // Adjust width based on number of weights
     ) {
         fun drawSquare(value: Float, index: Int) {
-            val color = when {
-                value > 0 -> Color.Green
-                value < 0 -> Color.Red
-                else -> Color.Gray
-            }
+            val color = colorMap.getColor(value)
             drawRect(
                 color = color,
                 topLeft = Offset(index * squareSize, 0f),
@@ -69,5 +66,53 @@ fun NeuronView(
             drawSquare(weight, index)
         }
         drawSquare(bias, weights.size)
+    }
+}
+
+fun interface ColorMap {
+    fun getColor(value: Float): Color
+}
+
+object DefaultColorMap : ColorMap {
+    override fun getColor(value: Float): Color {
+        return when {
+            value > 0 -> Color.Green
+            value < 0 -> Color.Red
+            else -> Color.Gray
+        }
+    }
+}
+
+data class CenteredColorMap(
+    val magnitudeClip: Float = 1.0f,
+    val centerColor: Color = Color.Gray,
+    val positiveColor: Color = Color.Green,
+    val negativeColor: Color = Color.Red,
+): ColorMap {
+    init {
+        require(magnitudeClip > 0) { "Magnitude clip must be greater than 0" }
+        require(centerColor != positiveColor && centerColor != negativeColor) {
+            "Center color must be different from positive and negative colors"
+        }
+        require(positiveColor != negativeColor) { "Positive and negative colors must be different" }
+    }
+
+    private fun interpolateColor(start: Color, end: Color, fraction: Float): Color {
+        return Color(
+            red = start.red + (end.red - start.red) * fraction,
+            green = start.green + (end.green - start.green) * fraction,
+            blue = start.blue + (end.blue - start.blue) * fraction,
+            alpha = start.alpha + (end.alpha - start.alpha) * fraction
+        )
+    }
+
+    override fun getColor(value: Float): Color {
+        val clippedValue = value.coerceIn(-magnitudeClip, magnitudeClip)
+        val normValue = clippedValue / magnitudeClip
+        return when {
+            clippedValue > 0 -> interpolateColor(centerColor, positiveColor, normValue)
+            clippedValue < 0 -> interpolateColor(centerColor, negativeColor, -normValue)
+            else -> centerColor
+        }
     }
 }
