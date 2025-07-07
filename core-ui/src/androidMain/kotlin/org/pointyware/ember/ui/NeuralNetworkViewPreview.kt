1a61751800a95d0a9d76d63cbfaa890d4b5bee78
Taush Sampley <taushsampley@gmail.com>
2025-07-07 13:47:33 -0500

    Add neural network preview

---
+++ core-ui/src/androidMain/kotlin/org/pointyware/ember/ui/NeuralNetworkViewPreview.kt
@@ -0,0 +1,44 @@
+package org.pointyware.ember.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.pointyware.ember.entities.activations.ReLU
+import org.pointyware.ember.entities.activations.Sigmoid
+import org.pointyware.ember.viewmodels.LayerUiState
+import org.pointyware.ember.viewmodels.NeuralNetworkUiState
+
+@Preview
+@Composable
+private fun NeuralNetworkViewPreview() {
+    NeuralNetworkView(
+        state = NeuralNetworkUiState(
+            layers = listOf(
+                LayerUiState(
+                    activationFunction = ReLU,
+                    weights = listOf(
+                        listOf(0.1f, 0.2f),
+                        listOf(-0.3f, 0.4f),
+                        listOf(0.5f, 0.6f)
+                    ),
+                    biases = listOf(0.1f, -0.2f, 0.3f),
+                ),
+                LayerUiState(
+                    activationFunction = Sigmoid,
+                    weights = listOf(
+                        listOf(-0.7f, 0.8f),
+                        listOf(-0.1f, -0.2f)
+                    ),
+                    biases = listOf(0.1f, 0.2f),
+                ),
+                LayerUiState(
+                    activationFunction = Sigmoid,
+                    weights = listOf(
+                        listOf(-0.7f, 0.8f),
+                        listOf(-0.1f, -0.2f)
+                    ),
+                    biases = listOf(0.1f, 0.2f),
+                )
+            )
+        )
+    )
+}
