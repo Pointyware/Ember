f60226d0aaf24ab072500ffd00047261d798b6bc
Taush Sampley <taushsampley@gmail.com>
2025-07-07 14:21:31 -0500

    Move NeuronView to dedicated file

--- core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/LayerView.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/LayerView.kt
@@ -38,35 +38,3 @@ fun LayerView(
         }
     }
 }
-
-/**
- * Displays a single neuron in a neural network layer.
- * The neuron is represented by its weights and bias
- * rendered as colored squares.
- */
-@Composable
-fun NeuronView(
-    weights: List<Float>,
-    bias: Float,
-    modifier: Modifier = Modifier,
-    squareSize: Float = 20f,
-    colorMap: ColorMap = DefaultColorMap
-) {
-    Canvas(
-        modifier = modifier.width(((weights.size + 1) * 20).dp) // Adjust width based on number of weights
-    ) {
-        fun drawSquare(value: Float, index: Int) {
-            val color = colorMap.getColor(value)
-            drawRect(
-                color = color,
-                topLeft = Offset(index * squareSize, 0f),
-                size = Size(squareSize, squareSize)
-            )
-        }
-        // Draw each weight as a colored square
-        weights.forEachIndexed { index, weight ->
-            drawSquare(weight, index)
-        }
-        drawSquare(bias, weights.size)
-    }
-}
---
+++ core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/NeuronView.kt
@@ -0,0 +1,43 @@
+package org.pointyware.ember.ui
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.dp
+import org.pointyware.ember.viewmodels.ColorMap
+import org.pointyware.ember.viewmodels.DefaultColorMap
+
+/**
+ * Displays a single neuron in a neural network layer.
+ * The neuron is represented by its weights and bias
+ * rendered as colored squares.
+ */
+@Composable
+fun NeuronView(
+    weights: List<Float>,
+    bias: Float,
+    modifier: Modifier = Modifier,
+    squareSize: Float = 20f,
+    colorMap: ColorMap = DefaultColorMap
+) {
+    Canvas(
+        modifier = modifier.width(((weights.size + 1) * 20).dp) // Adjust width based on number of weights
+    ) {
+        fun drawSquare(value: Float, index: Int) {
+            val color = colorMap.getColor(value)
+            drawRect(
+                color = color,
+                topLeft = Offset(index * squareSize, 0f),
+                size = Size(squareSize, squareSize)
+            )
+        }
+        // Draw each weight as a colored square
+        weights.forEachIndexed { index, weight ->
+            drawSquare(weight, index)
+        }
+        drawSquare(bias, weights.size)
+    }
+}
