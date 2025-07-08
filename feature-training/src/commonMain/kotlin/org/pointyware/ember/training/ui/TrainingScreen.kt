851d7ba0d41528d2b825a689a83d392493b2299b
Taush Sampley <taushsampley@gmail.com>
2025-07-08 15:13:15 -0500

    remove unused imports

--- feature-training/src/commonMain/kotlin/org/pointyware/ember/training/ui/TrainingScreen.kt
+++ feature-training/src/commonMain/kotlin/org/pointyware/ember/training/ui/TrainingScreen.kt
@@ -1,20 +1,9 @@
 package org.pointyware.ember.training.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import org.pointyware.ember.training.viewmodels.TrainingUiState
 import org.pointyware.ember.training.viewmodels.TrainingViewModel
-import org.pointyware.ember.ui.NeuralNetworkView
 
 /**
  * The Training Screen
