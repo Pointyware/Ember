738ef650e363c0ad5c782b36239117be6b1f74e4
Taush Sampley <taushsampley@gmail.com>
2025-07-08 18:41:50 -0500

    Fix CrossEntropyLoss using Double

--- core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/loss/CrossEntropyLoss.kt
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/loss/CrossEntropyLoss.kt
@@ -3,13 +3,19 @@ package org.pointyware.ember.entities.loss
 import org.pointyware.ember.entities.tensors.Tensor
 import kotlin.math.ln
 
+
+/**
+ * Also known as pseudo-count in population statistics.
+ */
+private const val EPSILON = 1e-15f
+
 /**
  * Cross-entropy loss function.
  */
 object CrossEntropyLoss : LossFunction {
     override fun compute(expected: Tensor, actual: Tensor): Double {
         val logProbs = Tensor.zeros(*actual.dimensions).mapEachFlatIndexed { index, _ ->
-            -ln(actual[index] + 1e-15) * expected[index]
+            -ln(actual[index] + EPSILON) * expected[index]
         }
 
         return logProbs.values.asSequence().sum() / actual.area.toDouble()
@@ -23,7 +29,7 @@ object CrossEntropyLoss : LossFunction {
         }
 
         return Tensor(actual.dimensions).mapEachFlatIndexed { index, _ ->
-            -expected[index] / (actual[index] + 1e-15)
+            -expected[index] / (actual[index] + EPSILON)
         }
     }
 
@@ -39,8 +45,8 @@ object CrossEntropyLoss : LossFunction {
         }
 
         val logProbs = Tensor.zeros(*actual.dimensions).mapEachFlatIndexed { index, _ ->
-            derivative[index] = -expected[index] / (actual[index] + 1e-15)
-            -ln(actual[index] + 1e-15) * expected[index]
+            derivative[index] = -expected[index] / (actual[index] + EPSILON)
+            -ln(actual[index] + EPSILON) * expected[index]
         }
 
         return logProbs.values.asSequence().sum() / actual.area.toDouble()
