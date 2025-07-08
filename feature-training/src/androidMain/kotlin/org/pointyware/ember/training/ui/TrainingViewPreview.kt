e3d4fbe0f1a131efc262a886ee34a157c6d5a37f
Taush Sampley <taushsampley@gmail.com>
2025-07-08 16:30:58 -0500

    Add statistics state

--- feature-training/src/androidMain/kotlin/org/pointyware/ember/training/ui/TrainingViewPreview.kt
+++ feature-training/src/androidMain/kotlin/org/pointyware/ember/training/ui/TrainingViewPreview.kt
@@ -3,43 +3,57 @@ package org.pointyware.ember.training.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import org.pointyware.ember.entities.activations.ReLU
+import org.pointyware.ember.training.viewmodels.StatisticsUiState
 import org.pointyware.ember.training.viewmodels.TrainingUiState
+import org.pointyware.ember.ui.theme.EmberTheme
+import org.pointyware.ember.viewmodels.DefaultColorMap
 import org.pointyware.ember.viewmodels.LayerUiState
 import org.pointyware.ember.viewmodels.NeuralNetworkUiState
 
 @Preview
 @Composable
 private fun TrainingViewPreview() {
-    TrainingView(
-        state = TrainingUiState(
-            isTraining = false,
-            epochsTrained = 10,
-            epochsRemaining = 20,
-            networkState = NeuralNetworkUiState(
-                layers = listOf(
-                    LayerUiState(
-                        activationFunction = ReLU,
-                        weights = listOf(
-                            listOf(0.1f, 0.2f),
-                            listOf(-0.3f, 0.4f),
-                            listOf(0.5f, 0.6f)
+    val colorMap = DefaultColorMap
+    EmberTheme {
+        TrainingView(
+            state = TrainingUiState(
+                isTraining = false,
+                epochsTrained = 10,
+                epochsRemaining = 20,
+                networkState = NeuralNetworkUiState(
+                    layers = listOf(
+                        LayerUiState(
+                            activationFunction = ReLU,
+                            weights = listOf(
+                                listOf(0.1f, 0.2f),
+                                listOf(-0.3f, 0.4f),
+                                listOf(0.5f, 0.6f)
+                            ),
+                            biases = listOf(0.1f, -0.2f, 0.3f),
+                            colorMap = colorMap
                         ),
-                        biases = listOf(0.1f, -0.2f, 0.3f),
-                    ),
-                    LayerUiState(
-                        activationFunction = ReLU,
-                        weights = listOf(
-                            listOf(-0.7f, 0.8f),
-                            listOf(-0.1f, -0.2f)
-                        ),
-                        biases = listOf(0.1f, 0.2f),
+                        LayerUiState(
+                            activationFunction = ReLU,
+                            weights = listOf(
+                                listOf(-0.7f, 0.8f),
+                                listOf(-0.1f, -0.2f)
+                            ),
+                            biases = listOf(0.1f, 0.2f),
+                            colorMap = colorMap
+                        )
                     )
+                ),
+                statistics = StatisticsUiState(
+                    objectiveName = "Accuracy",
+                    epochCount = 10,
+                    ceiling = 0.5f,
+                    data = emptyList()
                 )
-            )
-        ),
-        onStart = {},
-        onStop = {},
-        onReset = {},
-        onRun = {},
-    )
+            ),
+            onStart = {},
+            onStop = {},
+            onReset = {},
+            onRun = {},
+        )
+    }
 }
