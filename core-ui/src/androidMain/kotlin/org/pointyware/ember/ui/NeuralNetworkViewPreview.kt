69c90442fff507b988e3680b107049c070b919dd
Taush Sampley <taushsampley@gmail.com>
2025-07-08 14:54:45 -0500

    Wrap neural network view in ember theme for padding

--- core-ui/src/androidMain/kotlin/org/pointyware/ember/ui/NeuralNetworkViewPreview.kt
+++ core-ui/src/androidMain/kotlin/org/pointyware/ember/ui/NeuralNetworkViewPreview.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import org.pointyware.ember.entities.activations.ReLU
 import org.pointyware.ember.entities.activations.Sigmoid
+import org.pointyware.ember.ui.theme.EmberTheme
 import org.pointyware.ember.viewmodels.CenteredColorMap
 import org.pointyware.ember.viewmodels.LayerUiState
 import org.pointyware.ember.viewmodels.NeuralNetworkUiState
@@ -14,38 +15,40 @@ private fun NeuralNetworkViewPreview() {
     val consistentScale = CenteredColorMap(
         magnitudeClip = 0.7f,
     )
-    NeuralNetworkView(
-        state = NeuralNetworkUiState(
-            layers = listOf(
-                LayerUiState(
-                    activationFunction = ReLU,
-                    weights = listOf(
-                        listOf(0.1f, 0.2f, 0.5f),
-                        listOf(-0.3f, 0.4f, -0.6f),
-                        listOf(0.5f, 0.6f, 0.7f)
+    EmberTheme {
+        NeuralNetworkView(
+            state = NeuralNetworkUiState(
+                layers = listOf(
+                    LayerUiState(
+                        activationFunction = ReLU,
+                        weights = listOf(
+                            listOf(0.1f, 0.2f, 0.5f),
+                            listOf(-0.3f, 0.4f, -0.6f),
+                            listOf(0.5f, 0.6f, 0.7f)
+                        ),
+                        biases = listOf(0.1f, -0.2f, 0.3f),
+                        colorMap = consistentScale,
                     ),
-                    biases = listOf(0.1f, -0.2f, 0.3f),
-                    colorMap = consistentScale,
+                    LayerUiState(
+                        activationFunction = Sigmoid,
+                        weights = listOf(
+                            listOf(-0.7f, 0.8f),
+                            listOf(-0.1f, -0.2f)
+                        ),
+                        biases = listOf(0.1f, 0.2f),
+                        colorMap = consistentScale,
+                    ),
+                    LayerUiState(
+                        activationFunction = Sigmoid,
+                        weights = listOf(
+                            listOf(-0.7f, 0.8f),
+                            listOf(-0.1f, -0.2f)
+                        ),
+                        biases = listOf(0.1f, 0.2f),
+                        colorMap = consistentScale,
+                    )
                 ),
-                LayerUiState(
-                    activationFunction = Sigmoid,
-                    weights = listOf(
-                        listOf(-0.7f, 0.8f),
-                        listOf(-0.1f, -0.2f)
-                    ),
-                    biases = listOf(0.1f, 0.2f),
-                    colorMap = consistentScale,
-                ),
-                LayerUiState(
-                    activationFunction = Sigmoid,
-                    weights = listOf(
-                        listOf(-0.7f, 0.8f),
-                        listOf(-0.1f, -0.2f)
-                    ),
-                    biases = listOf(0.1f, 0.2f),
-                    colorMap = consistentScale,
-                )
-            ),
+            )
         )
-    )
+    }
 }
