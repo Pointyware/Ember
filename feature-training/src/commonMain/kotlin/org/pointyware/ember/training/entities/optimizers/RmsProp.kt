9f86dc92767664aa60c04beb16a536729a462338
Taush Sampley <taushsampley@gmail.com>
2025-07-09 14:52:42 -0500

    Add stubbed RmsProp

---
+++ feature-training/src/commonMain/kotlin/org/pointyware/ember/training/entities/optimizers/RmsProp.kt
@@ -0,0 +1,20 @@
+package org.pointyware.ember.training.entities.optimizers
+
+import org.pointyware.ember.entities.layers.Layer
+import org.pointyware.ember.entities.tensors.Tensor
+import org.pointyware.ember.training.entities.Exercise
+
+/**
+ * Implemented per research
+ */
+class RmsProp(
+
+): Optimizer {
+    override fun batch(cases: List<Exercise>): List<List<Exercise>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(layer: Layer, weightGradients: Tensor, biasGradients: Tensor) {
+        TODO("Not yet implemented")
+    }
+}
