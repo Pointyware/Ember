3ec7b0dee55fcd982dd15e064116ecc8609fb8fa
Taush Sampley <taushsampley@gmail.com>
2025-07-10 12:58:45 -0500

    Add other key optimizers

---
+++ feature-training/src/commonMain/kotlin/org/pointyware/ember/training/entities/optimizers/AdaGrad.kt
@@ -0,0 +1,47 @@
+package org.pointyware.ember.training.entities.optimizers
+
+import org.pointyware.ember.entities.layers.Layer
+import org.pointyware.ember.entities.tensors.Tensor
+import org.pointyware.ember.training.entities.Exercise
+
+class AdaGrad: StatisticalOptimizer {
+    override fun onEpochStart(epoch: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBatchStart(batch: List<Exercise>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSampleStart(sample: Exercise) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCost(cost: Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onGradient() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSampleEnd(sample: Exercise) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBatchEnd(batch: List<Exercise>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEpochEnd(epoch: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun batch(cases: List<Exercise>): List<List<Exercise>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(layer: Layer, weightGradients: Tensor, biasGradients: Tensor) {
+        TODO("Not yet implemented")
+    }
+}
