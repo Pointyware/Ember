0f620da5666238b573ad931aaff031170941cbd1
Taush Sampley <taushsampley@gmail.com>
2025-07-13 22:02:13 -0500

    Add future sum layer

---
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/layers/SumLayer.kt
@@ -0,0 +1,10 @@
+package org.pointyware.ember.entities.layers
+
+import org.pointyware.ember.entities.tensors.Tensor
+
+/**
+ *
+ * TODO: implement Layer interface after graph calculations
+ */
+class SumLayer {
+}
