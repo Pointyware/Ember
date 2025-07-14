1cc31dbdf30fd74ad32612bd4ee7fdc4195ef797
Taush Sampley <taushsampley@gmail.com>
2025-07-13 21:43:41 -0500

    Add comments for proposed network replacements

--- core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/networks/Network.kt
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/networks/Network.kt
@@ -13,6 +13,30 @@ import org.pointyware.ember.entities.tensors.Tensor
  * fun updateWeights(residuals: Residuals)
  */
 interface Network {
+//
+//    /**
+//     * Returns a map of calculation tensors identified by [Long] ids. Ids usually
+//     * correspond to layers; however, some networks have intermediate calculations
+//     * in addition to layers which can also be retrieved by id.
+//     */
+//    fun prepare(): Map<Long, Tensor>
+//
+//    /**
+//     * Represents one or more edges in a calculation graph that produce
+//     * single output in terms of one or more inputs.
+//     */
+//    data class Calculation(val inputs: List<Long>, val output: Long)
+//    /**
+//     * Returns a list of forward pass calculations in depth-first order
+//     * to guarantee all dependencies are ready.
+//     */
+//    fun forwardGraph(): List<Calculation>
+//    /**
+//     * Returns a list of backward pass calculations in depth-first order
+//     * to guarantee all dependencies are ready.
+//     */
+//    fun backwardGraph(): List<Calculation>
+
     /**
      * Takes the input and performs a forward pass through the network, returning the output
      * as a [Tensor].
@@ -37,4 +61,7 @@ interface Network {
         weightGradients: List<Tensor>,
         biasGradients: List<Tensor>
     )
+
+    fun forward(inputs: List<Tensor>, output: Tensor, calculations: Map<Long, Tensor>)
+    fun backward(errors: List<Tensor>, output: Tensor, calculations: Map<Long, Tensor>)
 }
