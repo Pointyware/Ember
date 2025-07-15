0bb7c362d73dcf3a23e3a6bfdc7fcb83f63e8170
Taush Sampley <taushsampley@gmail.com>
2025-07-15 01:22:33 -0500

    Move layer calculations out of network into layers

--- core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/networks/SequentialNetwork.kt
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/networks/SequentialNetwork.kt
@@ -41,19 +41,25 @@ open class SequentialNetwork(
         weightGradients: List<Tensor>,
         biasGradients: List<Tensor>
     ) {
-        // ∂^L = (f^L)' \dot (a^L - y);
+        // ∂_L = (f^L)' \dot (a_L - y);
         var layerError: Tensor = derivativeActivations.last() * error
         layers.indices.reversed().forEach back_pass@ { layerIndex ->
             val inputs = if (layerIndex == 0) { input } else { activations[layerIndex - 1] }
-            weightGradients[layerIndex] += layerError.matrixMultiply(inputs.transpose())
-            biasGradients[layerIndex] += layerError
 
             // Skip the first layer as it has no previous layer to propagate error to
-            if (layerIndex == 0) return@back_pass
-            // Propagate the error to the previous layer
-            // ∂^l-1 = (f^l-1)' \dot (W^l)^T \dot; ∂^l
-            layerError = derivativeActivations[layerIndex - 1] *
-                    layers[layerIndex].weights.transpose().matrixMultiply(layerError)
+            val priorActivationDerivative = if (layerIndex == 0) {
+                Tensor.ones(input.dimensions)
+            } else {
+                derivativeActivations[layerIndex - 1]
+            }
+            val priorError = Tensor(inputs.dimensions)
+            layers[layerIndex].backward( // TODO: replace with calculation graph traversal and context map
+                layerError,
+                inputs, priorActivationDerivative,
+                weightGradients[layerIndex], biasGradients[layerIndex],
+                priorError
+            )
+            layerError = priorError
         }
     }
 
