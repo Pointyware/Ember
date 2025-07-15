6f3acf781366df503ec53ff629940e7f2ecd059a
Taush Sampley <taushsampley@gmail.com>
2025-07-15 04:14:37 -0500

    Implement spiral classification problem

--- feature-training/src/commonMain/kotlin/org/pointyware/ember/training/data/SpiralExerciseGenerator.kt
+++ feature-training/src/commonMain/kotlin/org/pointyware/ember/training/data/SpiralExerciseGenerator.kt
@@ -2,14 +2,33 @@ package org.pointyware.ember.training.data
 
 import org.pointyware.ember.entities.tensors.columnVector
 import org.pointyware.ember.training.entities.Exercise
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
 
 /**
- *
+ * Generates a batch of spiral classification exercises.
  */
 class SpiralExerciseGenerator(
-    val problem: Problem.SpiralClassificationProblem
+    val problem: Problem.SpiralClassificationProblem,
+    val batchSize: Int = 50,
 ): ExerciseGenerator {
     override fun generate(): List<Exercise> {
-        TODO("Generate Spirals")
+        return List(batchSize) { index ->
+            val classId = index % 2
+            val phaseOffset = if (classId == 0) {
+                0.0
+            } else {
+                Math.PI
+            }
+            val t = Random.Default.nextFloat()
+            val theta = 2 * Math.PI * t + phaseOffset
+            val x = t * cos(theta) * problem.xMagnitude
+            val y = t * sin(theta) * problem.yMagnitude
+            Exercise(
+                input = columnVector(x.toFloat(), y.toFloat()),
+                output = columnVector(classId.toFloat()),
+            )
+        }
     }
 }
