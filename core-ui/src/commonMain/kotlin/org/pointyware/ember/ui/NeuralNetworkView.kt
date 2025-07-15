e2f2e086c38b6b7ee991985921810e03919f720d
Taush Sampley <taushsampley@gmail.com>
2025-07-15 03:33:29 -0500

    Move network view implementation to lazy composable and create static view

--- core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/NeuralNetworkView.kt
+++ core-ui/src/commonMain/kotlin/org/pointyware/ember/ui/NeuralNetworkView.kt
@@ -1,6 +1,7 @@
 package org.pointyware.ember.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -14,6 +15,24 @@ import org.pointyware.ember.viewmodels.NeuralNetworkUiState
  */
 @Composable
 fun NeuralNetworkView(
+    state: NeuralNetworkUiState,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(EmberTheme.geometry.paddingSmall)
+    ) {
+        state.layers.forEach { layerState ->
+            LayerView(
+                state = layerState,
+                modifier = Modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun LazyNeuralNetworkView(
     state: NeuralNetworkUiState,
     modifier: Modifier = Modifier
 ) {
