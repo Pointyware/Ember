8044afb7aa8d1713da836cdc67af249abbea9bf1
Taush Sampley <taushsampley@gmail.com>
2025-07-17 11:37:24 -0500

    Fix crash where dataSeries are empty

--- feature-training/src/commonMain/kotlin/org/pointyware/ember/training/viewmodels/StatisticsUiStateMapper.kt
+++ feature-training/src/commonMain/kotlin/org/pointyware/ember/training/viewmodels/StatisticsUiStateMapper.kt
@@ -12,12 +12,12 @@ object StatisticsUiStateMapper: Mapper<Snapshot, StatisticsUiState> {
     override fun map(input: Snapshot): StatisticsUiState {
         val dataSeries = dataSeriesMapper.map(input.measurements)
         val max = if (dataSeries.isNotEmpty()) {
-            dataSeries.maxOf { series -> series.dataPoints.maxOf { it.second } }
+            dataSeries.maxOf { series -> if (series.dataPoints.isNotEmpty()) series.dataPoints.maxOf { it.second } else 0f }
         } else {
             10f
         }
         val min = if (dataSeries.isNotEmpty()) {
-            dataSeries.maxOf { series -> series.dataPoints.minOf { it.second } }
+            dataSeries.maxOf { series -> if (series.dataPoints.isNotEmpty()) series.dataPoints.minOf { it.second } else 0f }
         } else {
             0f
         }
