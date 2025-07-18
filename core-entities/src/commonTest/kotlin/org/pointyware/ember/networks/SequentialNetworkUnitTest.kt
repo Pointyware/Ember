4bd465ac3ebbab89177de8c0d1b763cbe19d8d6d
Taush Sampley <taushsampley@gmail.com>
2025-07-18 16:54:03 -0500

    Move SequentialNetworkUnitTest to core-entities module

--- core-entities/src/commonTest/kotlin/org/pointyware/ember/networks/SequentialNetworkUnitTest.kt
+++ core-entities/src/commonTest/kotlin/org/pointyware/ember/networks/SequentialNetworkUnitTest.kt
@@ -12,7 +12,7 @@ class SequentialNetworkUnitTest {
     @Test
     fun sequential_network_passes_input_through_layers() {
         val layer1 = LinearLayer(
-            weights = Tensor.zeros(2, 3).apply {
+            weights = Tensor.Companion.zeros(2, 3).apply {
                 this[0, 0] = 2.0f
                 this[0, 1] = 3.0f
                 this[0, 2] = 5.0f
@@ -20,24 +20,24 @@ class SequentialNetworkUnitTest {
                 this[1, 1] = 11.0f
                 this[1, 2] = 13.0f
             },
-            biases = Tensor.zeros(2, 1).apply {
+            biases = Tensor.Companion.zeros(2, 1).apply {
                 this[0, 0] = 17.0f
                 this[1, 0] = 19.0f
             },
             activationFunction = ReLU
         )
         val layer2 = LinearLayer(
-            weights = Tensor.zeros(1, 2).apply {
+            weights = Tensor.Companion.zeros(1, 2).apply {
                 this[0, 0] = 23.0f
                 this[0, 1] = 27.0f
             },
-            biases = Tensor.zeros(1, 1).apply {
+            biases = Tensor.Companion.zeros(1, 1).apply {
                 this[0, 0] = 29.0f
             },
             activationFunction = ReLU
         )
         val network = SequentialNetwork(listOf(layer1, layer2))
-        val input = Tensor.zeros(3, 1).apply {
+        val input = Tensor.Companion.zeros(3, 1).apply {
             this[0] = 2.0f
             this[1] = 3.0f
             this[2] = 5.0f
@@ -46,11 +46,11 @@ class SequentialNetworkUnitTest {
         // The output should be a tensor of shape (1, 1) since the last layer has 1 output neuron
         assert(output.dimensions.contentEquals(intArrayOf(1, 1))) { "Output shape should be (1, 1), but was ${output.shapeString}" }
 
-        val layer1Output = Tensor.zeros(2, 1).apply {
+        val layer1Output = Tensor.Companion.zeros(2, 1).apply {
             this[0, 0] = 2.0f * 2.0f +  3.0f * 3.0f +  5.0f * 5.0f + 17.0f
             this[1, 0] = 7.0f * 2.0f + 11.0f * 3.0f + 13.0f * 5.0f + 19.0f
         }
-        val expected = Tensor.zeros(1, 1).apply {
+        val expected = Tensor.Companion.zeros(1, 1).apply {
             this[0, 0] =
                 layer1Output[0, 0] * 23.0f + layer1Output[1, 0] * 27.0f + 29.0f
         }
