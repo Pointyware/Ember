cc6db26712efff1e74d94d11c96daa70608f95ee
Taush Sampley <taushsampley@gmail.com>
2025-07-19 17:27:42 -0500

    Add scaled dot product attention with optional mask

---
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/blocks/ScaledDotProductAttention.kt
@@ -0,0 +1,38 @@
+package org.pointyware.ember.entities.blocks
+
+import org.pointyware.ember.entities.activations.SoftMax
+import org.pointyware.ember.entities.tensors.Tensor
+import kotlin.math.pow
+
+/**
+ *
+ * - Scaled Dot Product Attention x Head Count (h)
+ *   - MatMul Q and K
+ *   - Scale by sqrt(d_k)
+ *   - Optional Mask
+ *   - SoftMax
+ *   - MatMul SoftMax output with V
+ *
+ * @param dimensionKey The dimension of the key and query inputs.
+ * @param dimensionValue The dimension of the value input.
+ * @param mask An optional mask to apply to the attention scores.
+ */
+class ScaledDotProductAttention(
+    val dimensionKey: Int,
+    val dimensionValue: Int,
+    val mask: Tensor? = null
+): Attention {
+
+    private val softmax = SoftMax()
+
+    private val scalingFactor = dimensionKey.toDouble().pow(-0.5).toFloat()
+    override fun invoke(query: Tensor, key: Tensor, value: Tensor): Tensor {
+        val matMul = query.matrixMultiply(key)
+        matMul.mapEach { it * scalingFactor }
+        if (mask != null) {
+            matMul.mapEachFlatIndexed { index, value -> value * mask[index] }
+        }
+        val softmax = softmax.calculate(matMul)
+        return softmax.matrixMultiply(value)
+    }
+}
