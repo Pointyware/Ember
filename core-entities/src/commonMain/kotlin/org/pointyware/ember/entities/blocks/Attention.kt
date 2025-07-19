7ccc073078ae416787658796c80d768602bc99c8
Taush Sampley <taushsampley@gmail.com>
2025-07-19 17:26:28 -0500

    Add attention block type

---
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/blocks/Attention.kt
@@ -0,0 +1,12 @@
+package org.pointyware.ember.entities.blocks
+
+import org.pointyware.ember.entities.tensors.Tensor
+
+/**
+ * An attention mechanism takes in query, key, and value tensors and returns a weighted
+ * sum of the values, where the weights are calculated based on compatibility between
+ * the query and key.
+ */
+interface Attention: Block {
+    fun invoke(query: Tensor, key: Tensor, value: Tensor): Tensor
+}
