c51ef7d8190cbca077284ee9d4e6dab6f924126d
Taush Sampley <taushsampley@gmail.com>
2025-07-19 22:16:16 -0500

    Add agent to simulation module

---
+++ feature-simulation/src/commonMain/kotlin/org/pointyware/ember/simulation/Agent.kt
@@ -0,0 +1,17 @@
+package org.pointyware.ember.simulation
+
+import org.pointyware.ember.entities.signals.Signal
+
+/**
+ * Agents can exist at any scale of the system. Their body plan determines how they
+ * interact with an environment, including how collisions stimulate different
+ * senses, and how their actions affect the environment.
+ *
+ * For each time step of the simulation, each input to the senses is updated,
+ * followed by an internal processing model, which determines the action outputs,
+ * innervating the body, which acts on the environment.
+ */
+interface Agent {
+    val senses: List<Signal>
+    val actions: List<Signal>
+}
