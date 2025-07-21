e0e36692972e94241af47a4d558afd8e5b7d210c
Taush Sampley <taushsampley@gmail.com>
2025-07-21 15:05:10 -0500

    Implement genetics mutation

--- feature-evolution/src/commonMain/kotlin/org/pointyware/ember/evolution/Genetics.kt
+++ feature-evolution/src/commonMain/kotlin/org/pointyware/ember/evolution/Genetics.kt
@@ -1,6 +1,6 @@
 package org.pointyware.ember.evolution
 
-import org.pointyware.ember.entities.networks.Network
+import kotlin.random.Random
 
 /**
  * @param ribosomeSequence The sequence that determines the ribosome structure.
@@ -10,5 +10,49 @@ class Genetics(
     val ribosomeSequence: ByteArray,
     val modelSequence: ByteArray
 ) {
+    private val entropy = Random.Default
 
+    /**
+     *
+     */
+    fun mutate(mutationRate: Double): Genetics {
+        return Genetics(
+            ribosomeSequence = mutateBytes(ribosomeSequence, mutationRate),
+            modelSequence = mutateBytes(modelSequence, mutationRate)
+        )
+    }
+
+    private val insertionError = 1.0
+    private val deletionError = 1.0
+    private val substitutionError = 2.0
+    private val mutationError = insertionError + deletionError + substitutionError
+
+    private fun mutateBytes(sequence: ByteArray, mutationRate: Double): ByteArray {
+        val mutationCount = (sequence.size * mutationRate).toInt()
+
+        val newSequence = sequence.copyOf()
+        for (i in 0 until mutationCount) {
+            val mutationType = entropy.nextDouble(mutationError)
+            val index = entropy.nextInt(0, sequence.size)
+            when {
+                mutationType < insertionError -> {
+                    TODO("Implement insertion error")
+                }
+                mutationType < deletionError -> {
+                    TODO("Implement deletion error")
+                }
+                else -> { // mutation < substitutionError
+                    newSequence[index] = entropy.nextInt(from = 0, until = 4).toByte()
+                }
+            }
+        }
+        return newSequence
+    }
+
+    companion object {
+        const val baseCount: Int = 4
+        const val codonSize = 3
+        const val deletionByte =  0x001000.toByte()
+        const val insertionByte = 0x011000.toByte()
+    }
 }
