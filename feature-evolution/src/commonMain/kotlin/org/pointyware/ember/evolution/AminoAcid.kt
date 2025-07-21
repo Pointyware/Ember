b5ca9e5d8992d8e10bc2c998c19e0c2106220172
Taush Sampley <taushsampley@gmail.com>
2025-07-21 15:04:38 -0500

    Add amino acid shortcodes

--- feature-evolution/src/commonMain/kotlin/org/pointyware/ember/evolution/AminoAcid.kt
+++ feature-evolution/src/commonMain/kotlin/org/pointyware/ember/evolution/AminoAcid.kt
@@ -4,30 +4,32 @@ package org.pointyware.ember.evolution
  * These names are included for user convenience. They are listed in GACU order.
  * For example GGG, GGA, ..., ACA, ACC, ..., UUA, UUC, UUU.
  */
-enum class AminoAcid {
-    Glycine,
-    GlutamicAcid,
-    AsparticAcid,
-    Alanine,
-    Valine,
-    Arginine,
-    Serine,
-    Lysine,
-    Asparagine,
-    Threonine,
-    Methionine,
-    Isoleucine,
+enum class AminoAcid(
+    val shortCode: String,
+) {
+    Glycine("G"),
+    GlutamicAcid("Ga"),
+    AsparticAcid("A"),
+    Alanine("Al"),
+    Valine("V"),
+    Arginine("Ar"),
+    Serine("S"),
+    Lysine("L"),
+    Asparagine("As"),
+    Threonine("Th"),
+    Methionine("Me"),
+    Isoleucine("Is"),
     // Arginine Duplicate Coding
-    Glutamine,
-    Histidine,
-    Proline,
-    Leucine,
-    Tryptophan,
+    Glutamine("Gl"),
+    Histidine("H"),
+    Proline("P"),
+    Leucine("Le"),
+    Tryptophan("Tr"),
     // Stop Gap
-    Cysteine,
+    Cysteine("C"),
     // Stop Gap
-    Tyrosine,
+    Tyrosine("Ty"),
     // Serine Duplicate Coding
     // Leucine Duplicate Coding
-    Phenylalanine;
+    Phenylalanine("Ph");
 }
