afa958935c979c6ed0f666b7fa90cef5b6e4dee7
Taush Sampley <taushsampley@gmail.com>
2025-07-21 13:03:18 -0500

    Create ProteinDynamics class to handle protein folding and complex dynamics

---
+++ feature-evolution/src/commonMain/kotlin/org/pointyware/ember/evolution/ProteinDynamics.kt
@@ -0,0 +1,16 @@
+package org.pointyware.ember.evolution
+
+import org.pointyware.ember.entities.networks.Network
+
+/**
+ *
+ */
+class ProteinDynamics {
+
+    /**
+     *
+     */
+    fun doTheThing(aminoAcidChains: List<List<AminoAcid>>): Network {
+        TODO("Implement")
+    }
+}
