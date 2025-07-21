e6988f4403ccca9a0a93c37a1ff2a06a9a7a9776
Taush Sampley <taushsampley@gmail.com>
2025-07-21 13:01:59 -0500

    Add amino acids enumeration

---
+++ feature-evolution/src/commonMain/kotlin/org/pointyware/ember/evolution/AminoAcid.kt
@@ -0,0 +1,32 @@
+package org.pointyware.ember.evolution
+
+/**
+ * These names are included for user convenience.
+ */
+enum class AminoAcid {
+    Glycine,
+    GlutamicAcid,
+    AsparticAcid,
+    Alanine,
+    Valine,
+    Arginine,
+    Serine,
+    Lysine,
+    Asparagine,
+    Threonine,
+    Methionine,
+    Isoleucine,
+    // Arginine Duplicate Coding
+    Glutamine,
+    Histidine,
+    Proline,
+    Leucine,
+    Tryptophan,
+    // Stop Gap
+    Cysteine,
+    // Stop Gap
+    Tyrosine,
+    // Serine Duplicate Coding
+    // Leucine Duplicate Coding
+    Phenylalanine,
+}
