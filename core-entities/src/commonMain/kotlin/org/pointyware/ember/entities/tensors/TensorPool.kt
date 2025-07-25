fd3567e27f81becacc036b18307d764ef3cba8f4
Taush Sampley <taushsampley@gmail.com>
2025-07-25 16:33:51 -0500

    Add TensorPool documentation

--- core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/tensors/TensorPool.kt
+++ core-entities/src/commonMain/kotlin/org/pointyware/ember/entities/tensors/TensorPool.kt
@@ -3,7 +3,7 @@ package org.pointyware.ember.entities.tensors
 import org.pointyware.ember.entities.ObjectPool
 
 /**
- *
+ * A pool of [Tensor] instances. Each tensor is zeroed when it is returned to the pool.
  */
 class TensorPool: ObjectPool<IntArray, Tensor>() {
     override fun getKeyFromObject(obj: Tensor): IntArray {
