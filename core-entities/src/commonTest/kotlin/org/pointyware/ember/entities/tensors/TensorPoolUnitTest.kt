fc9a00b2085ad6327588b9538fd5841a96ce90bf
Taush Sampley <taushsampley@gmail.com>
2025-07-25 16:39:41 -0500

    Add TensorPoolUnitTest

---
+++ core-entities/src/commonTest/kotlin/org/pointyware/ember/entities/tensors/TensorPoolUnitTest.kt
@@ -0,0 +1,15 @@
+package org.pointyware.ember.entities.tensors
+
+import kotlin.test.Test
+
+class TensorPoolUnitTest {
+    @Test
+    fun test_same_dimensions_return_same_object() {
+        val pool = TensorPool()
+        val tensor1 = pool.getObject(listOf(1, 2, 3))
+        pool.returnObject(tensor1)
+
+        val tensor2 = pool.getObject(listOf(1, 2, 3))
+        assert(tensor1 === tensor2)
+    }
+}
