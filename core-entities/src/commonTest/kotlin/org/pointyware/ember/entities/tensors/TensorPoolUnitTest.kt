05d5c8d5fa53822f2a4026e71905d2e725a4ce27
Taush Sampley <taushsampley@gmail.com>
2025-07-25 16:41:22 -0500

    Add assertion to TensorPoolUnitTest

--- core-entities/src/commonTest/kotlin/org/pointyware/ember/entities/tensors/TensorPoolUnitTest.kt
+++ core-entities/src/commonTest/kotlin/org/pointyware/ember/entities/tensors/TensorPoolUnitTest.kt
@@ -10,6 +10,8 @@ class TensorPoolUnitTest {
         pool.returnObject(tensor1)
 
         val tensor2 = pool.getObject(listOf(1, 2, 3))
-        assert(tensor1 === tensor2)
+        assert(tensor1 === tensor2) { "Returned objects should be reused when possible" }
+        val tensor3 = pool.getObject(listOf(1, 2, 4))
+        assert(tensor1 !== tensor3) { "Returned objects should not be reused after being retrieved" }
     }
 }
