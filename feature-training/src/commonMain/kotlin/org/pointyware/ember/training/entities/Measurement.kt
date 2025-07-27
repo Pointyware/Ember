6606823fe5495d0980d8d26822ea87fde64e4153
Taush Sampley <taushsampley@gmail.com>
2025-07-27 15:58:37 -0500

    Convert measurement to sealed class

--- feature-training/src/commonMain/kotlin/org/pointyware/ember/training/entities/Measurement.kt
+++ feature-training/src/commonMain/kotlin/org/pointyware/ember/training/entities/Measurement.kt
@@ -8,43 +8,45 @@ package org.pointyware.ember.training.entities
  * - Intermediate information, a.k.a. calculations, such as pre-activations, activations, derivatives, loss, gradients, etc.
  * - Analytical information, a.k.a. calculations on calculations, such as
  */
-sealed interface Measurement {
+sealed class Measurement(
+//    val name: String
+) {
     /**
      * Collects the weights of the model for an epoch.
      */
-    data object Weights: Measurement
+    data object Weights: Measurement()
     /**
      * Collects all biases of the model for an epoch.
      */
-    data object AllParameters: Measurement
+    data object AllParameters: Measurement()
 
     /**
      * Collects the weighted and biased inputs of the model, for each sample in the epoch. `z = Wx + b`
      */
-    data object Preactivation: Measurement
+    data object Preactivation: Measurement()
 
     /**
      * Collects the activations of the model, for each sample in the epoch. `a = f(z)`
      */
-    data object Activation: Measurement
+    data object Activation: Measurement()
 
     /**
      * Collects the derivatives of the model. `df/dz`
      */
-    data object Derivative: Measurement
+    data object Derivative: Measurement()
 
     /**
      * Collects the loss, a.k.a. cost, of the model, averaged across an epoch. `∑C(a, y)/N`
      */
-    data object Loss: Measurement
+    data object Loss: Measurement()
 
     /**
      * Collects the accuracy of the model, averaged across an epoch. `∑A(a, y)/N`
      */
-    data object Accuracy: Measurement
+    data object Accuracy: Measurement()
 
     /**
      * Collects the gradients of the model, averaged across an epoch. `df/dw`
      */
-    data object Gradient: Measurement
+    data object Gradient: Measurement()
 }
